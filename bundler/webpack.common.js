580739793c523f8ece70e7ca386ba6edcd75f278
unknown <2573424017@qq.com>
2021-10-12 16:21:00 +0800

    解决 资源文件bug

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -60,7 +60,8 @@ module.exports = {
                     options: {
                         outputPath: 'fonts'
                     }
-                }]
+                }],
+                type: 'javascript/auto'
             },
             // css
             {
