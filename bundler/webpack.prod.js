6ec445b297abee736c6fb63fb8fe7c7251255452
unknown <2573424017@qq.com>
2021-10-20 17:32:53 +0800

    10/20

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -1,16 +1,54 @@
-const {merge} = require('webpack-merge')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const {
+    merge
+} = require('webpack-merge')
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin')
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin')
+const webpack = require('webpack')
 
 const path = require('path')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
-    entry:path.resolve(__dirname,'../src/main.js'),
-    plugins:[
-        new CleanWebpackPlugin({})
+    entry: path.resolve(__dirname, '../src/main.js'),
+    plugins: [
+        new CleanWebpackPlugin({}),
+        new webpack.optimize.SplitChunksPlugin({
+
+        })
     ],
-    optimization:{
-        minimizer:[new CssMinimizerPlugin()]
+    optimization: {
+        minimize: true,
+        minimizer: [new CssMinimizerPlugin(),
+            new TerserPlugin({
+                test: /\.js(\?.*)?$/i,
+                terserOptions: {
+                    comments: false
+                }
+            }),
+        ],
+        splitChunks: {
+            chunks: 'all',
+            minSize: 20000,
+            minRemainingSize: 0,
+            minChunks: 1,
+            maxAsyncRequests: 30,
+            maxInitialRequests: 30,
+            enforceSizeThreshold: 50000,
+            cacheGroups: {
+                defaultVendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10,
+                    reuseExistingChunk: true,
+                },
+                default: {
+                    minChunks: 2,
+                    priority: -20,
+                    reuseExistingChunk: true,
+                },
+            },
+        },
     }
 })
