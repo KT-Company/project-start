8ba3854a6cd28bd2a3ac01248b3292c75852fca3
unknown <2573424017@qq.com>
2021-10-21 11:48:35 +0800

    temp

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -7,20 +7,33 @@ const path = require('path')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
-	entry: path.resolve(__dirname, '../src/main.js'),
-	plugins: [
-		new CleanWebpackPlugin({
-			path: './dist',
-		}),
-	],
-	optimization: {
-		minimizer: [
-			new CssMinimizerPlugin(),
-			new UglifyJsPlugin({
-				cache: true,
-				parallel: true,
-				sourceMap: true,
-			}),
-		],
-	},
+    entry: path.resolve(__dirname, '../src/main.js'),
+    plugins: [
+        new CleanWebpackPlugin({}),
+        new webpack.optimize.SplitChunksPlugin(),
+        new webpack.optimize.LimitChunkCountPlugin({
+            maxChunks: 6
+        }),
+        new webpack.optimize.MinChunkSizePlugin({
+            minChunkSize: 1000 * 200
+        }),
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [new CssMinimizerPlugin(),
+            new TerserPlugin({
+                test: /\.js(\?.*)?$/i,
+                extractComments: false,
+                terserOptions: {
+                    format: {
+                        comments: false,
+                    },
+                }
+            }),
+        ],
+        splitChunks: {
+            maxSize: 1000 * 2000,
+            chunks: 'all',
+        },
+    }
 })
